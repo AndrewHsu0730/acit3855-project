95e909848bb76248a6225fe1d60464892b6275aa
Andrew Hsu <andrew311000gmail.com>
2025-04-03 14:16:15 -0700

    Change the CORS setting

--- analyzer/app.py
+++ analyzer/app.py
@@ -5,6 +5,7 @@ from pykafka import KafkaClient
 import json
 from connexion.middleware import MiddlewarePosition
 from starlette.middleware.cors import CORSMiddleware
+import os
 
 
 with open("config/analyzer/app_conf.yaml", "r") as f:
@@ -83,15 +84,15 @@ def get_stats():
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 
-
-app.add_middleware(
-    CORSMiddleware,
-    position=MiddlewarePosition.BEFORE_EXCEPTION,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+if "CORS_ALLOW_ALL" in os.environ and os.environ["CORS_ALLOW_ALL"] == "yes":
+    app.add_middleware(
+        CORSMiddleware,
+        position=MiddlewarePosition.BEFORE_EXCEPTION,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+    )
 
 
 app.add_api("bulkup.yaml", base_path = "/analyzer", strict_validation = True, validate_responses = True)
